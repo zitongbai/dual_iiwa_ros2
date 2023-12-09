559a883ac61c9e4effb450cf7d0ad872ac85036d
xiaobaigeLAB <zitongbai@outlook.com>
2023-12-09 23:12:09 +0800

    dual iiwa in gazebo

---
+++ dual_iiwa_bringup/launch/dual_iiwa.launch.py
@@ -0,0 +1,425 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, RegisterEventHandler
+from launch.conditions import IfCondition, UnlessCondition
+from launch.event_handlers import OnProcessExit, OnProcessStart
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'runtime_config_package',
+            default_value='dual_iiwa_description',
+            description='Package with the controller\'s configuration in "config" folder. \
+                         Usually the argument is not set, it enables use of a custom setup.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'controllers_file',
+            default_value='dual_iiwa_controllers.yaml',
+            description='YAML file with the controllers configuration.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'description_package',
+            default_value='dual_iiwa_description',
+            description='Description package with robot URDF/xacro files. Usually the argument \
+                         is not set, it enables use of a custom description.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'description_file',
+            default_value='dual_iiwa.urdf.xacro',
+            description='URDF/XACRO description file with the robot.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'prefix',
+            default_value='""',
+            description='Prefix of the joint names, useful for multi-robot setup. \
+                         If changed than also joint names in the controllers \
+                         configuration have to be updated. Expected format "<prefix>/"',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'namespace',
+            default_value='',
+            description='Namespace of launched nodes, useful for multi-robot setup. \
+                         If changed than also the namespace in the controllers \
+                         configuration needs to be updated. Expected format "<ns>/".',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_sim',
+            default_value='true',
+            description='Start robot in Gazebo simulation.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_fake_hardware',
+            default_value='true',
+            description='Start robot with fake hardware mirroring command to its states.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_planning',
+            default_value='false',
+            description='Start robot with Moveit2 `move_group` planning \
+                         config for Pilz and OMPL.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_servoing',
+            default_value='false',
+            description='Start robot with Moveit2 servoing.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'left_robot_controller',
+            default_value='left_iiwa_arm_controller',
+            description='Robot controller to start.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'right_robot_controller',
+            default_value='right_iiwa_arm_controller',
+            description='Robot controller to start.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'start_rviz',
+            default_value='true',
+            description='Start RViz2 automatically with this launch file.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'left_robot_ip',
+            default_value='192.170.10.1',
+            description='Robot IP of FRI interface',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'right_robot_ip',
+            default_value='192.170.10.2',
+            description='Robot IP of FRI interface',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'left_robot_port',
+            default_value='30200',
+            description='Robot port of FRI interface.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'right_robot_port',
+            default_value='30200',
+            description='Robot port of FRI interface.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'left_initial_positions_file',
+            default_value='left_initial_positions.yaml',
+            description='Configuration file of robot initial positions for simulation.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'right_initial_positions_file',
+            default_value='right_initial_positions.yaml',
+            description='Configuration file of robot initial positions for simulation.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'command_interface',
+            default_value='position',
+            description='Robot command interface [position|velocity|effort].',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'base_frame_file',
+            default_value='base_frame.yaml',
+            description='Configuration file of robot base frame wrt World.',
+        )
+    )
+
+    # Initialize Arguments
+    runtime_config_package = LaunchConfiguration('runtime_config_package')
+    controllers_file = LaunchConfiguration('controllers_file')
+    description_package = LaunchConfiguration('description_package')
+    description_file = LaunchConfiguration('description_file')
+    prefix = LaunchConfiguration('prefix')
+    use_sim = LaunchConfiguration('use_sim')
+    use_fake_hardware = LaunchConfiguration('use_fake_hardware')
+    use_planning = LaunchConfiguration('use_planning')
+    use_servoing = LaunchConfiguration('use_servoing')
+    left_robot_controller = LaunchConfiguration('left_robot_controller')
+    right_robot_controller = LaunchConfiguration('right_robot_controller')
+    start_rviz = LaunchConfiguration('start_rviz')
+    left_robot_ip = LaunchConfiguration('left_robot_ip')
+    left_robot_port = LaunchConfiguration('left_robot_port')
+    right_robot_ip = LaunchConfiguration('right_robot_ip')
+    right_robot_port = LaunchConfiguration('right_robot_port')
+    left_initial_positions_file = LaunchConfiguration('left_initial_positions_file')
+    right_initial_positions_file = LaunchConfiguration('right_initial_positions_file')
+    command_interface = LaunchConfiguration('command_interface')
+    base_frame_file = LaunchConfiguration('base_frame_file')
+    namespace = LaunchConfiguration('namespace')
+
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
+            PathJoinSubstitution(
+                [FindPackageShare(description_package), 'config', description_file]
+            ),
+            ' ',
+            'prefix:=',
+            prefix,
+            ' ',
+            'use_sim:=',
+            use_sim,
+            ' ',
+            'use_fake_hardware:=',
+            use_fake_hardware,
+            ' ',
+            'left_robot_ip:=',
+            left_robot_ip,
+            ' ',
+            'left_robot_port:=',
+            left_robot_port,
+            ' ',
+            'left_initial_positions_file:=',
+            left_initial_positions_file,
+            ' ',
+            'right_robot_ip:=',
+            right_robot_ip,
+            ' ',
+            'right_robot_port:=',
+            right_robot_port,
+            ' ',
+            'right_initial_positions_file:=',
+            right_initial_positions_file,
+            ' ',
+            'command_interface:=',
+            command_interface,
+            ' ',
+            'base_frame_file:=',
+            base_frame_file,
+            ' ',
+            # 'description_package:=',
+            # description_package, # This parameter should be the description package of single iiwa robot
+            # ' ', 
+            'runtime_config_package:=',
+            runtime_config_package,
+            ' ',
+            'controllers_file:=',
+            controllers_file,
+            ' ',
+            'namespace:=',
+            namespace,
+        ]
+    )
+
+    robot_description = {'robot_description': robot_description_content}
+
+    # Running with Moveit2 planning
+    iiwa_planning_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            FindPackageShare('iiwa_bringup'),
+            '/launch',
+            '/iiwa_planning.launch.py'
+        ]),
+        launch_arguments={
+            'description_package': description_package,
+            'description_file': description_file,
+            'prefix': prefix,
+            'start_rviz': start_rviz,
+            'base_frame_file': base_frame_file,
+            'namespace': namespace,
+            'use_sim': use_sim,
+        }.items(),
+        condition=IfCondition(use_planning),
+    )
+
+    # Running with Moveit2 servoing
+    iiwa_servoing_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            FindPackageShare('iiwa_bringup'),
+            '/launch',
+            '/iiwa_servoing.launch.py'
+        ]),
+        launch_arguments={
+            'description_package': description_package,
+            'description_file': description_file,
+            'prefix': prefix,
+            'base_frame_file': base_frame_file,
+            'namespace': namespace,
+        }.items(),
+        condition=IfCondition(use_servoing),
+    )
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare(runtime_config_package),
+            'config',
+            controllers_file,
+        ]
+    )
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(description_package), 'rviz', 'dual_iiwa.rviz']
+    )
+
+    control_node = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[robot_description, robot_controllers],
+        output='both',
+        namespace=namespace,
+        condition=UnlessCondition(use_sim), # Only when using real robot. Gazebo will use libgazebo_ros2_control.so
+    )
+    robot_state_pub_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        namespace=namespace,
+        output='both',
+        parameters=[robot_description],
+    )
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='log',
+        arguments=['-d', rviz_config_file],
+        parameters=[
+            robot_description,
+        ],
+        condition=UnlessCondition(use_planning),
+    )
+    iiwa_simulation_world = PathJoinSubstitution(
+        [FindPackageShare(description_package),
+            'gazebo/worlds', 'empty.world']
+    )
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [PathJoinSubstitution(
+                [FindPackageShare('gazebo_ros'),
+                    'launch', 'gazebo.launch.py']
+            )]
+        ),
+        launch_arguments={'verbose': 'false', 'world': iiwa_simulation_world}.items(),
+        condition=IfCondition(use_sim),
+    )
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', [namespace, 'robot_description'], '-entity', [namespace, 'iiwa14']],
+        output='screen',
+        condition=IfCondition(use_sim),
+    )
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster', '--controller-manager',
+                   [namespace, 'controller_manager']],
+    )
+
+    left_external_torque_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['left_ets_state_broadcaster', '--controller-manager',
+                   [namespace, 'controller_manager']],
+        condition=UnlessCondition(use_sim), # Only in real robot
+    )
+    right_external_torque_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['right_ets_state_broadcaster', '--controller-manager',
+                   [namespace, 'controller_manager']],
+        condition=UnlessCondition(use_sim), # Only in real robot
+    )
+    left_robot_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[left_robot_controller, '--controller-manager', [namespace, 'controller_manager']],
+    )
+    right_robot_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[right_robot_controller, '--controller-manager', [namespace, 'controller_manager']],
+    )
+
+    # Delay `joint_state_broadcaster` after spawn_entity
+    delay_joint_state_broadcaster_spawner_after_spawn_entity = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[joint_state_broadcaster_spawner],
+        ),
+        condition=IfCondition(use_sim),
+    )
+
+    # Delay `joint_state_broadcaster` after control_node
+    delay_joint_state_broadcaster_spawner_after_control_node = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=control_node,
+            on_start=[joint_state_broadcaster_spawner],
+        ),
+        condition=UnlessCondition(use_sim),
+    )
+
+    # Delay rviz start after `joint_state_broadcaster`
+    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[rviz_node],
+        ),
+        condition=IfCondition(start_rviz),
+    )
+
+    # Delay start of robot_controller after `joint_state_broadcaster`
+    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[right_robot_controller_spawner, left_robot_controller_spawner],
+        )
+    )
+
+    nodes = [
+        gazebo,
+        control_node,
+        iiwa_planning_launch,
+        iiwa_servoing_launch,
+        spawn_entity,
+        robot_state_pub_node,
+        delay_joint_state_broadcaster_spawner_after_control_node,
+        delay_joint_state_broadcaster_spawner_after_spawn_entity,
+        delay_rviz_after_joint_state_broadcaster_spawner,
+        left_external_torque_broadcaster_spawner,
+        right_external_torque_broadcaster_spawner,
+        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+    ]
+
+    return LaunchDescription(declared_arguments + nodes)
